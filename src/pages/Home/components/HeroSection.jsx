660d02a0f3173ff48b3727559d40c1a9dfa2929d
Piyush Soni <Piiyush.sonii@outlook.com>
2025-06-21 08:51:52 +0000

    deploying on github pages

--- src/pages/Home/components/HeroSection.jsx
+++ src/pages/Home/components/HeroSection.jsx
@@ -5,7 +5,7 @@ import image1 from "../../../assets/image1.png";
 import image2 from "../../../assets/image2.png";
 import image3 from "../../../assets/image3.png";
 import image4 from "../../../assets/image4.png";
-import image5 from "../../../assets/image5.png";
+import image5 from "../../../assets/Image5.png";
 import image6 from "../../../assets/image6.png";
 import image7 from "../../../assets/image7.png";
 import image8 from "../../../assets/image8.png";
